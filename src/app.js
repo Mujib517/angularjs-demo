2a5b7ea3433fd6b06227065bbf28fd3c021cef71
Mujib517 <Mujib517@gmail.com>
2018-10-30 08:00:21 +0530

    Routing, Filters

--- src/app.js
+++ src/app.js
@@ -1,2 +1,24 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
 
+app.config(function ($routeProvider) {
+
+  $routeProvider
+    .when('/', {
+      template: '<h1>Home Page!!</h1>'
+    })
+    .when('/about', {
+      template: '<h1>About Page</h1>'
+    })
+    .when('/contact', {
+      template: '<h1>Contact Page</h1>'
+    })
+    .when('/users', {
+      templateUrl: '/src/templates/users.html',
+      controller: 'userCtrl'
+    })
+    .when('/products', {
+      templateUrl: '/src/templates/products.html',
+      controller: 'productsCtrl'
+    })
+    .otherwise({ redirectTo: '/' });
+});
---
+++ src/productsCtrl.js
@@ -0,0 +1,13 @@
+app.controller('productsCtrl', function ($scope, $http) {
+
+  $http.get('http://exp-rest-api.herokuapp.com/api/products/0/100')
+    .then(function (res) {
+      $scope.products = res.data.data;
+      console.log(res.data.data);
+    })
+    .catch(function (err) { console.log(err) });
+
+  $scope.getRelativeTime = function (date) {
+    return moment(date).fromNow();
+  }
+});
