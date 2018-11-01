af9c1e9191e88acc77d3f36f38405cbb8de92cfd
Mujib517 <Mujib517@gmail.com>
2018-11-01 07:36:07 +0530

    Delete Product

--- src/product.svc.js
+++ src/product.svc.js
@@ -13,4 +13,8 @@ function ProductSvc($http) {
   this.save = function (product) {
     return $http.post('http://exp-rest-api.herokuapp.com/api/products', product);
   }
+
+  this.delete = function (id) {
+    return $http.delete('http://exp-rest-api.herokuapp.com/api/products/' + id);
+  }
 }
--- src/productsCtrl.js
+++ src/productsCtrl.js
@@ -1,8 +1,22 @@
 app.controller('productsCtrl', function ($scope, productSvc) {
 
-  productSvc.get()
-    .then(function (res) {
-      $scope.products = res.data.data;
-    })
-    .catch(function (err) { console.log(err) });
+  function init() {
+    productSvc.get()
+      .then(function (res) {
+        $scope.products = res.data.data;
+      })
+      .catch(function (err) { console.log(err) });
+  }
+
+  $scope.onDelete = function (id) {
+    productSvc.delete(id)
+      .then(function (res) {
+        init();
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+
+  init();
 });
